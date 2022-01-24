fb06bdcc8116481a043d8569f30d48766d981add
GustBitencourt <gubiten@gmail.com>
2022-01-24 13:52:27 -0300

    rota test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const api = express();
+
+api.use('/test', (req: Request, res: Response) => {
+    res.json({ sucess: true });
+
+})
 
 api.listen(5000, () => console.log('Tô Ouvindo!!'));
